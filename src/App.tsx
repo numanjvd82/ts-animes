9fb1c14d1741f09bc8651c048d8cdeb8ad60b4d6
Numan <nomanmani62@gmail.com>
2022-11-08 00:21:42 +0500

    routes and navbar is complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import { Routes, Route } from 'react-router-dom';
+import Login from './components/Login/Login';
 import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
-    <div className="bg-blue-400 h-full">
-      <Navbar />
+    <div className="bg-blue-300 h-screen">
+      <Routes>
+        <Route path="/" element={<Navbar />}>
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<h1>404</h1>} />
+        </Route>
+      </Routes>
     </div>
   );
 }
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,10 +1,36 @@
+import { Link, Outlet } from 'react-router-dom';
+import logo from '../../assets/images/gundam.png';
+
 function Navbar() {
   return (
-    <nav className="sticky top-0 z-10 bg-white p-4">
-      <ul>
-        <li></li>
-      </ul>
-    </nav>
+    <>
+      <nav className="bg-blue-900 px-6 h-14 w-full ">
+        <div className="flex justify-between items-center pt-1">
+          <figure className="w-12">
+            <Link to="/">
+              <img className="object-cover" src={logo} alt="Anime Logo" />
+            </Link>
+          </figure>
+
+          <article className="flex justify-center items-center">
+            <input
+              className="mr-2 w-64 h-8 rounded-md text-sm text-gray-700 placeholder-gray-400 border border-gray-300 focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500"
+              name="search-anime"
+              type="text"
+              placeholder="Search Animes"
+            />
+            <figure>
+              <Link to="/login">
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition-colors">
+                  Login
+                </button>
+              </Link>
+            </figure>
+          </article>
+        </div>
+      </nav>
+      <Outlet />
+    </>
   );
 }
 
