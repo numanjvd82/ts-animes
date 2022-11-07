8b86757848cbb179e67381f6e241db9659dced9f
Numan <nomanmani62@gmail.com>
2022-11-07 23:08:36 +0500

    setup a react app with vite

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Navbar from './components/Navbar/Navbar';
+
+function App() {
+  return (
+    <div className="bg-blue-400 h-full">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+function Navbar() {
+  return (
+    <nav className="sticky top-0 z-10 bg-white p-4">
+      <ul>
+        <li></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
