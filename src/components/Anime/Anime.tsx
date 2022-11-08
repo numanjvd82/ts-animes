7e71d134af3381c6802ecbad72ce735107b629a8
Numan <nomanmani62@gmail.com>
2022-11-09 00:11:58 +0500

    some effects on images

--- src/components/Anime/Anime.tsx
+++ src/components/Anime/Anime.tsx
@@ -1,14 +1,15 @@
 import { Anime as AnimeType } from '../../slices/animeSlice';
+import { Link } from 'react-router-dom';
 
 function Anime({ anime }: { anime: AnimeType }) {
   const { attributes } = anime;
   return (
     <section className=" bg-white m-4 relative rounded-t-md rounded-b-md w-[350px] md:w-[250px]">
       <article className="">
-        <figure className=" cursor-pointer object-cover">
+        <figure className="overflow-hidden cursor-pointer object-cover">
           <img
             src={attributes.posterImage.large}
-            className="w-full h-[300px] rounded-t-md hover:opacity-80 transition duration-500 ease-in-out"
+            className="w-full h-[300px] rounded-t-md hover:opacity-80 hover:scale-125 transition duration-500 ease-in-out"
             alt={attributes.titles.en}
           />
         </figure>
@@ -22,7 +23,7 @@ function Anime({ anime }: { anime: AnimeType }) {
             {attributes.synopsis.slice(0, 100)}...
           </p>
           <button className="text-sm mt-3 text-gray-800 font-semibold bg-blue-300 p-1 rounded-md transition-colors hover:bg-blue-500 hover:text-gray-700">
-            Read More
+            <Link to={`/anime/${anime.id}`}>Read More</Link>
           </button>
         </article>
       </article>
