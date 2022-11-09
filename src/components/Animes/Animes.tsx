7bcfefbd77eecf1e878a0cc727b8db2449b0e3cd
Numan <nomanmani62@gmail.com>
2022-11-09 20:19:27 +0500

    used our custom hook to get the animes

--- src/components/Animes/Animes.tsx
+++ src/components/Animes/Animes.tsx
@@ -1,31 +1,34 @@
-import { useEffect, useState } from 'react';
-import { useAppDispatch, useAppSelector } from '../../hooks/hooks';
-import { selectAnimes, fetchAnimes } from '../../slices/animeSlice';
-
 import Loader from '../Loader/Loader';
 import Anime from '../Anime/Anime';
-import useScroll from '../../hooks/useScroll';
+import useAnimes from '../../hooks/useAnimes';
 
 function Animes() {
-  const scroll = useScroll();
-  console.log(scroll);
-  const animes = useAppSelector(selectAnimes);
-  const dispatch = useAppDispatch();
+  const { animes, hasMore } = useAnimes();
 
-  const [offset, setOffset] = useState(0);
-
-  useEffect(() => {
-    dispatch(fetchAnimes(offset));
-  }, [offset]);
+  const noMore = !hasMore ? (
+    <div className="flex justify-center items-center">
+      <h1 className="text-3xl text-white my-2 bg-blue-500 p-2 rounded-md transition-all hover:-translate-y-2 hover:bg-blue-700">
+        <a
+          onClick={() => {
+            window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+          }}
+          href="#top"
+        >
+          No more animes...
+        </a>
+      </h1>
+    </div>
+  ) : null;
 
   return (
-    <section className="md:max-w-[1500px] mx-auto">
+    <section id="top" className="md:max-w-[1500px] mx-auto">
+      {animes.loading === 'pending' && <Loader />}
       <section className="md:flex md:flex-wrap">
-        {animes.loading === 'pending' && <Loader />}
         {animes.loading === 'failed' && <h1>Failed to load</h1>}
         {animes.loading === 'success' &&
           animes.animes.map((anime) => <Anime key={anime.id} anime={anime} />)}
       </section>
+      {noMore}
     </section>
   );
 }
