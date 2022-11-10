6446693613bddc6075eb724e7de1f89cec3ba955
Numan <nomanmani62@gmail.com>
2022-11-10 21:49:12 +0500

    wrapped our login route with our form context provider

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Animes from './components/Animes/Animes';
 import Login from './components/Login/Login';
 import Navbar from './components/Navbar/Navbar';
 import SingleAnime from './components/SingleAnime/SingleAnime';
+import { FormContextProvider } from './context/FormContext';
 
 function App() {
   return (
@@ -10,7 +11,14 @@ function App() {
       <Routes>
         <Route path="/" element={<Navbar />}>
           <Route path="/" element={<Animes />} />
-          <Route path="/login" element={<Login />} />
+          <Route
+            path="/login"
+            element={
+              <FormContextProvider>
+                <Login />
+              </FormContextProvider>
+            }
+          />
           <Route path="/anime/:id" element={<SingleAnime />} />
           <Route path="*" element={<h1>404</h1>} />
         </Route>
