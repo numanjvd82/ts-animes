8eb2beb524ea45ff47ec2469a7a9a817c6447f2e
Numan <nomanmani62@gmail.com>
2022-11-14 00:48:26 +0500

    fetch animes by searching

--- src/hooks/useAnimes.tsx
+++ src/hooks/useAnimes.tsx
@@ -1,11 +1,17 @@
 import { useCallback, useEffect, useRef, useState } from 'react';
-import { fetchAnimes, selectAnimes } from '../slices/animeSlice';
+import {
+  fetchAnimeBySearch,
+  fetchAnimes,
+  searchQuery,
+  selectAnimes,
+} from '../slices/animeSlice';
 import { useAppDispatch, useAppSelector } from './hooks';
 
 const useAnimes = () => {
   const [offset, setOffset] = useState(0);
   const [hasMore, setHasMore] = useState(true);
   const animes = useAppSelector(selectAnimes);
+  const search = useAppSelector(searchQuery);
   const dispatch = useAppDispatch();
 
   const limit = 100;
@@ -13,6 +19,12 @@ const useAnimes = () => {
   useEffect(() => {
     dispatch(fetchAnimes(offset));
   }, [offset]);
+
+  useEffect(() => {
+    if (search) {
+      dispatch(fetchAnimeBySearch(search));
+    }
+  }, [search]);
 
   const intObserver = useRef<IntersectionObserver | null>(null);
   const lastAnimeRef = useCallback(
