5af3b7491a49f118043f3078d38f3f68bbb3aeb9
Numan <nomanmani62@gmail.com>
2022-11-14 00:48:48 +0500

    storing search value in redux store

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,16 +2,10 @@ import { useState } from 'react';
 import { Link, Outlet } from 'react-router-dom';
 import logo from '../../assets/images/gundam.png';
 import { useAppDispatch, useAppSelector } from '../../hooks/hooks';
-import {
-  fetchAnimeBySearch,
-  searchQuery,
-  setSearchQuery,
-} from '../../slices/animeSlice';
+import { searchQuery, setSearchQuery } from '../../slices/animeSlice';
 
 function Navbar() {
-  const [expanded, setExpanded] = useState(false);
-
-  const search = useAppSelector(searchQuery);
+  const [text, setText] = useState('');
   const dispatch = useAppDispatch();
 
   return (
@@ -25,16 +19,24 @@ function Navbar() {
           </figure>
 
           <article className="flex justify-center items-center">
-            <input
-              className="mr-2 w-64 h-8 rounded-md text-sm text-gray-700 placeholder-gray-400 border border-gray-300 focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500"
-              name="search-anime"
-              type="text"
-              placeholder="Search Animes"
-              value={search}
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                dispatch(setSearchQuery(e.target.value))
-              }
-            />
+            <form
+              onSubmit={(e: React.ChangeEvent<HTMLFormElement>) => {
+                e.preventDefault();
+                dispatch(setSearchQuery(text));
+              }}
+            >
+              <input
+                className="mr-2 w-64 h-8 rounded-md text-sm text-gray-700 placeholder-gray-400 border border-gray-300 focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500"
+                name="search-anime"
+                id="search"
+                type="text"
+                placeholder="Search Animes"
+                value={text}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                  setText(e.target.value);
+                }}
+              />
+            </form>
             <figure>
               <Link to="/login">
                 <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition-colors">
