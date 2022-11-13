21b06db819a757459398b9dc677a4997b0135440
Numan <nomanmani62@gmail.com>
2022-11-14 00:24:54 +0500

    stored search state in redux store

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,27 +1,39 @@
 import { useState } from 'react';
 import { Link, Outlet } from 'react-router-dom';
 import logo from '../../assets/images/gundam.png';
+import { useAppDispatch, useAppSelector } from '../../hooks/hooks';
+import {
+  fetchAnimeBySearch,
+  searchQuery,
+  setSearchQuery,
+} from '../../slices/animeSlice';
 
 function Navbar() {
   const [expanded, setExpanded] = useState(false);
-  console.log(expanded);
+
+  const search = useAppSelector(searchQuery);
+  const dispatch = useAppDispatch();
+
   return (
     <>
       <nav className="bg-blue-900 px-6 py-3 w-full ">
-        <div className="flex justify-between items-center pt-1">
+        <div className="flex max-sm:flex-col justify-between items-center pt-1">
           <figure className="max-sm:mb-3 max-sm:w-14 w-16 p-2 rounded-full bg-blue-400 md:transition-all md:hover:bg-blue-500 cursor-pointer md:hover:scale-105">
             <Link to="/">
               <img className="object-cover" src={logo} alt="Anime Logo" />
             </Link>
           </figure>
 
-          {/* For big screens */}
           <article className="flex justify-center items-center">
             <input
               className="mr-2 w-64 h-8 rounded-md text-sm text-gray-700 placeholder-gray-400 border border-gray-300 focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500"
               name="search-anime"
               type="text"
               placeholder="Search Animes"
+              value={search}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                dispatch(setSearchQuery(e.target.value))
+              }
             />
             <figure>
               <Link to="/login">
--- src/slices/animeSlice.tsx
+++ src/slices/animeSlice.tsx
@@ -26,13 +26,14 @@ type AnimeState = {
   animes: Anime[];
   loading: 'idle' | 'pending' | 'success' | 'failed';
   error: null;
+  searchQuery: string;
 };
 
 type PayloadActionType = PayloadAction<
   Anime[],
   string,
   {
-    arg?: number;
+    arg?: number | string;
     requestId: string;
     requestStatus: 'fulfilled';
   }
@@ -42,6 +43,7 @@ const initialState: AnimeState = {
   animes: [],
   loading: 'idle',
   error: null,
+  searchQuery: '',
 };
 
 export const fetchAnimes = createAsyncThunk(
@@ -80,10 +82,48 @@ export const fetchAnimes = createAsyncThunk(
   }
 );
 
+export const fetchAnimeBySearch = createAsyncThunk(
+  'animes/fetchBySearch',
+  async (search: string): Promise<Anime[]> => {
+    try {
+      const response = await fetch(`${baseUrl}?filter[text]=${search}`);
+      const data = await response.json();
+      const customData: Anime[] = data.data.map((anime: Anime) => {
+        return {
+          id: anime.id,
+          attributes: {
+            slug: anime.attributes.slug,
+            synopsis: anime.attributes.synopsis,
+            titles: {
+              en: anime.attributes.titles.en,
+              ja_jp: anime.attributes.titles.ja_jp,
+            },
+            status: anime.attributes.status,
+            posterImage: {
+              large: anime.attributes.posterImage.large,
+            },
+            averageRating: anime.attributes.averageRating,
+          },
+          youtubeVideoId: anime.attributes.youtubeVideoId,
+          coverImage: anime.attributes.coverImage,
+        };
+      });
+      return customData;
+    } catch (error) {
+      console.log(error);
+    }
+    return [];
+  }
+);
+
 export const animeSlice = createSlice({
   name: 'anime',
   initialState,
-  reducers: {},
+  reducers: {
+    setSearchQuery: (state, action: { payload: string; type: string }) => {
+      state.searchQuery = action.payload;
+    },
+  },
   extraReducers: (builder) => {
     builder.addCase(fetchAnimes.pending, (state) => {
       state.loading = 'pending';
@@ -104,9 +144,28 @@ export const animeSlice = createSlice({
     builder.addCase(fetchAnimes.rejected, (state) => {
       state.loading = 'failed';
     });
+
+    builder.addCase(fetchAnimeBySearch.pending, (state) => {
+      state.loading = 'pending';
+    });
+
+    builder.addCase(
+      fetchAnimeBySearch.fulfilled,
+      (state, action: PayloadActionType) => {
+        state.loading = 'success';
+        state.animes = action.payload;
+      }
+    );
+
+    builder.addCase(fetchAnimeBySearch.rejected, (state) => {
+      state.loading = 'failed';
+    });
   },
 });
 
 export const selectAnimes = (state: RootState) => state.animes;
+export const searchQuery = (state: RootState) => state.animes.searchQuery;
+
+export const { setSearchQuery } = animeSlice.actions;
 
 export default animeSlice.reducer;
